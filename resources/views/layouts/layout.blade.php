bec79368a91c1e07e902b536a442e239ff0a4fcb
ratsmusic2 <ratsmusic2@gmail.com>
2023-07-07 12:27:46 -0400

    nuevo con 3d

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -24,120 +24,130 @@
         <link href="./assets/css/paper-kit.css?v=2.3.0" rel="stylesheet"/>
         <!-- CSS Just for demo purpose, dont include it in your project -->
         <link href="./assets/demo/demo.css" rel="stylesheet"/>
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
     </head>
 
     <body class="sections-page " style="position:relative;min-height: 100vh;">
         <div id="wrapper" style="padding-bottom: 4rem; ">
-         <!-- Navbar -->
-  <nav
-  class="navbar navbar-expand-lg fixed-top nav-down navbar-light"
-  style="background-color:#ffffff">
-  <div class="container">
-      <div class="navbar-translate">
-        <a href="/">
-          <img src="./assets/img/Limitless.png" class="img-fluid " alt="...">
-        </a>
-          <!-- <a class="navbar-brand" href="/index.html" rel="tooltip" title="Limitless"
-          data-placement="bottom" target="_blank"> Limitless Desiiiign </a>-->
-          <button
-              class="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navigation"
-              aria-controls="navigation-index"
-              aria-expanded="false"
-              aria-label="Toggle navigation">
-              <span class="navbar-toggler-bar bar1"></span>
-              <span class="navbar-toggler-bar bar2"></span>
-              <span class="navbar-toggler-bar bar3"></span>
-          </button>
-      </div>
-      <div
-          class="collapse navbar-collapse"
-          data-nav-image="./assets/img/blurred-image-1.jpg"
-          data-color="orange">
-          <ul class="navbar-nav ml-auto">
-              <li class="dropdown nav-item">
-                  <a
-                      href="#"
-                      class="dropdown-toggle nav-link"
-                      data-toggle="dropdown"
-                      aria-expanded="false">
-                      DESIGNS
-                  </a>
-                  <div class="dropdown-menu dropdown-menu-right dropdown-danger">
-                      <a href="./3ddesing" class="dropdown-item">
-                          3D Designs
-                      </a>
-                      <a href="./logosdesing" class="dropdown-item">
-                          Logos Designs
-                      </a>
-                      <a href="./blueprintsdesing" class="dropdown-item">
-                          Blueprints Designs
-                      </a>
-                  </div>
-              </li>
-              <li class="dropdown nav-item">
-                  <a
-                      href="#"
-                      class="dropdown-toggle nav-link"
-                      id="navbarDropdownMenuLink"
-                      data-toggle="dropdown">
-                      Sections
-                  </a>
-                  <div
-                      class="dropdown-menu dropdown-menu-right dropdown-danger"
-                      aria-labelledby="navbarDropdownMenuLink">
-                      <a
-                          class="dropdown-item"
-                          data-scroll="true"
-                          data-id="#headers"
-                          href="./sections.html#headers">
-                          <i class="nc-icon nc-tile-56"></i>
-                          Headers
-                      </a>
-                  </div>
-              </li>
-              <li class="dropdown nav-item">
-                  <a
-                      href="#"
-                      class="dropdown-toggle nav-link"
-                      id="navbarDropdownMenuLink1"
-                      data-toggle="dropdown">
-                      Examples
-                  </a>
-                  <div
-                      class="dropdown-menu dropdown-menu-right dropdown-danger"
-                      aria-labelledby="navbarDropdownMenuLink1">
-                      <a class="dropdown-item" href="./examples/about-us.html">
-                          <i class="nc-icon nc-bank"></i>
-                          About-us
-                      </a>
+            <!-- Navbar -->
+            <nav
+                class="navbar navbar-expand-lg fixed-top nav-down navbar-light"
+                style="background-color:#ffffff">
+                <div class="container">
+                    <div class="navbar-translate">
+                        <a href="/">
+                            <img src="./assets/img/Limitless.png" class="img-fluid " alt="...">
+                        </a>
+                        <!-- <a class="navbar-brand" href="/index.html" rel="tooltip" title="Limitless"
+                        data-placement="bottom" target="_blank"> Limitless Desiiiign </a>-->
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navigation"
+                            aria-controls="navigation-index"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span class="navbar-toggler-bar bar1"></span>
+                            <span class="navbar-toggler-bar bar2"></span>
+                            <span class="navbar-toggler-bar bar3"></span>
+                        </button>
+                    </div>
+                    <div
+                        class="collapse navbar-collapse"
+                        data-nav-image="./assets/img/blurred-image-1.jpg"
+                        data-color="orange">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="dropdown nav-item">
+                                <a
+                                    href="#"
+                                    class="dropdown-toggle nav-link"
+                                    data-toggle="dropdown"
+                                    aria-expanded="false">
+                                    DESIGNS
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-danger">
+                                    <a href="./3ddesing" class="dropdown-item">
+                                        3D Designs
+                                    </a>
+                                    <a href="./logosdesing" class="dropdown-item">
+                                        Logos Designs
+                                    </a>
+                                    <a href="./blueprintsdesing" class="dropdown-item">
+                                        Blueprints Designs
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="dropdown nav-item">
+                                <a
+                                    href="#"
+                                    class="dropdown-toggle nav-link"
+                                    id="navbarDropdownMenuLink"
+                                    data-toggle="dropdown">
+                                    Sections
+                                </a>
+                                <div
+                                    class="dropdown-menu dropdown-menu-right dropdown-danger"
+                                    aria-labelledby="navbarDropdownMenuLink">
+                                    <a
+                                        class="dropdown-item"
+                                        data-scroll="true"
+                                        data-id="#headers"
+                                        href="./sections.html#headers">
+                                        <i class="nc-icon nc-tile-56"></i>
+                                        Headers
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="dropdown nav-item">
+                                <a
+                                    href="#"
+                                    class="dropdown-toggle nav-link"
+                                    id="navbarDropdownMenuLink1"
+                                    data-toggle="dropdown">
+                                    Examples
+                                </a>
+                                <div
+                                    class="dropdown-menu dropdown-menu-right dropdown-danger"
+                                    aria-labelledby="navbarDropdownMenuLink1">
+                                    <a class="dropdown-item" href="./examples/about-us.html">
+                                        <i class="nc-icon nc-bank"></i>
+                                        About-us
+                                    </a>
 
-                  </div>
-              </li>
-          </ul>
-      </div>
-  </div>
-</nav>
-<!-- End Navbar --> 
-<div class="section"></div>
-@yield('content')
-</div>
-<footer class="footer footer-black  footer-white " style="position: absolute;width:100%;bottom:0;height: 5rem;">
-    <div class="container">
-      <div class="row">
-        <div class="credits ml-auto">
-          <span class="copyriooght">
-            ©
-            <script>
-              document.write(new Date().getFullYear())
-            </script>, made wit <i class="fa fa-heart heart"></i> 
-          </span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- End Navbar -->
+            <div class="section"></div>
+            @yield('content')
         </div>
-      </div>
-    </div>
-  </footer>  
+        <footer
+            class="footer footer-black  footer-white "
+            style="position: absolute;width:100%;bottom:0;height: 5rem;">
+            <div class="container">
+                <div class="row">
+                    <div class="credits ml-auto">
+                        <span class="copyriooght">
+                            ©
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script>, made wit
+                            <i class="fa fa-heart heart"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </footer>
         <!-- Core JS Files -->
         <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
         <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
@@ -192,12 +202,12 @@
                 demo.initContactUsMap2();
             });
         </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
 
     </body>
-    
-</html>    
- 
 
-
-
-
+</html>
