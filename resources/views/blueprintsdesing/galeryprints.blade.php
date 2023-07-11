2ba1416e18c6518af965ab763d3fc9aa184dd668
ratsmusic2 <ratsmusic2@gmail.com>
2023-07-11 15:24:16 -0400

    ultimo

--- resources/views/blueprintsdesing/galeryprints.blade.php
+++ resources/views/blueprintsdesing/galeryprints.blade.php
@@ -70,7 +70,7 @@
     </div>
 </div>
 </div>
-<div class="photo-gallery">
+<div class="photo-gallery" >
     <div class="container">
         <div class="intro">
             <h2 class="text-center card-title">2D Color FloorPlans</h2>
@@ -78,18 +78,18 @@
             <p class="text-center card-description">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
         </div>
         <div class="container" style="max-width: 740px;">
-          <div id="carouselExampleIndicators" class="carousel slide  d-block mx-auto " data-ride="carousel" style="height: 510px;">
+          <div id="carouselExampleIndicators1" class="carousel slide  d-block mx-auto " data-ride="carousel" style="height: 510px;">
             <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="3"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="4"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="5"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="6"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="7"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="8"></li>
+              <li data-target="#carouselExampleIndicators1" data-slide-to="9"></li>
             </ol>
             <div class="carousel-inner">
               <div class="carousel-item active">
@@ -120,17 +120,17 @@
                 <img class="d-block w-100" src="./assets/img/exterior/13.jpg" alt="Third slide">
               </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <a class="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
             </a>
           </div>
         </div>
-        <div class="row photos">
+        <div class="row photos" >
             <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="./assets/img/farid-askerov.jpg" data-lightbox="photos"><img class="img-fluid" src="./assets/img/farid-askerov.jpg"></a></div>
             <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="./assets/img/farid-askerov.jpg" data-lightbox="photos"><img class="img-fluid" src="./assets/img/farid-askerov.jpg"></a></div>
             <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="./assets/img/farid-askerov.jpg" data-lightbox="photos"><img class="img-fluid" src="./assets/img/farid-askerov.jpg"></a></div>
@@ -151,15 +151,15 @@
                 <p class="text-center card-descrition">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
             </div>
             <div class="container" style="max-width: 740px;">
-              <div id="carouselExampleIndicators" class="carousel slide  d-block mx-auto " data-ride="carousel" style="height: 510px;">
+              <div id="carouselExampleIndicators2" class="carousel slide  d-block mx-auto " data-ride="carousel" style="height: 510px;">
                 <ol class="carousel-indicators">
-                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                  <li data-target="#carouselExampleIndicators2" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators2" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleIndicators2" data-slide-to="2"></li>
+                  <li data-target="#carouselExampleIndicators2" data-slide-to="3"></li>
+                  <li data-target="#carouselExampleIndicators2" data-slide-to="4"></li>
+                  <li data-target="#carouselExampleIndicators2" data-slide-to="5"></li>
+                  <li data-target="#carouselExampleIndicators2" data-slide-to="6"></li>
                 </ol>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
@@ -181,17 +181,17 @@
                     <img class="d-block w-100" src="./assets/img/planos/6.png" alt="Third slide">
                   </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <a class="carousel-control-prev" href="#carouselExampleIndicators2" role="button" data-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                   <span class="sr-only">Previous</span>
                 </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <a class="carousel-control-next" href="#carouselExampleIndicators2" role="button" data-slide="next">
                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
                   <span class="sr-only">Next</span>
                 </a>
               </div>
             </div>
-            <div class="row photos">
+            <div class="row photos" style="margin-bottom: 100px; margin-top: 150px;">
                 <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="./assets/img/planos/1.png" data-lightbox="photos"><img class="img-fluid" src="./assets/img/planos/1.png" ></a></div>
                 <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="./assets/img/planos/2.png" data-lightbox="photos"><img class="img-fluid" src="./assets/img/planos/2.png" ></a></div>
                 <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="./assets/img/planos/3.png" data-lightbox="photos"><img class="img-fluid" src="./assets/img/planos/3.png" ></a></div>
